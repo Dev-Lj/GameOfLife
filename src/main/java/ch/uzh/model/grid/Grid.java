42f014216630d56b3bf9650747b40d90b211e084
Atomis14 <michelsabbatini@outlook.com>
2022-12-21 20:00:56 +0100

    changed structure to implement grid

--- src/main/java/ch/uzh/model/grid/Grid.java
+++ src/main/java/ch/uzh/model/grid/Grid.java
@@ -6,18 +6,28 @@ import java.util.List;
 import ch.uzh.model.lobby.Player;
 
 public class Grid {
+    private static final int GRID_SIZE = 3;
+    private final Player[][] grid = new Player[GRID_SIZE][GRID_SIZE];
     List<GridObserver> observers = new ArrayList<>();
+
 
     public void changeGrid() {
         notifyObservers();
     }
 
-    public static void killCell(Grid grid, int x, int y, Player currentPlayer) {
-        // TODO implement
+    /**
+     * @pre coordinates not out of bounds of grid
+     * */
+    public void killCell(int x, int y, Player currentPlayer) {
+        assert x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE;
     }
 
-    public static void plantCell(Grid grid, int x, int y, Player currentPlayer) {
-        // TODO implement
+    public void plantCell(int x, int y, Player currentPlayer) {
+        assert x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE;
+    }
+
+    public void computeGeneration() {
+        // TODO: Implement
     }
 
     public void attachObserver(GridObserver observer) {
@@ -28,5 +38,10 @@ public class Grid {
         for (GridObserver gridObserver : observers) {
             gridObserver.notifyUpdate();
         }
+    }
+
+    // JUST FOR DEBUGGING PURPOSES
+    public static void main(String[] args) {
+        Grid gridInstance = new Grid();
     }
 }
