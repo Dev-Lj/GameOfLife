4abec2f0cd9818c7c36a6d73cf92ce2c3983b95d
nataell95 <nataell.cornu@uzh.ch>
2022-12-27 13:40:22 +0100

    added the css to the stylesheet folder and removed the css folder

--- src/main/java/ch/uzh/App.java
+++ src/main/java/ch/uzh/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("PlayerConfigView"), 800, 600);
-        scene.getStylesheets().add(getClass().getResource("css/PlayerConfig.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("stylesheets/PlayerConfig.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
